7053d7b2473c1314d8ccf8f6fd8fdfe7029840bc
malerey <reymalena@gmail.com>
2020-09-11 22:53:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const botonAbrirMenu = document.getElementById("abrir-menu")
+const botonCerrarMenu = document.getElementById("cerrar-menu")
+const menu = document.getElementById("menu")
+const overlay = document.getElementById("overlay")
+const gatito = document.getElementById("gatito")
+
+botonAbrirMenu.onclick = () => {
+  overlay.classList.remove("ocultar")
+}
+
+botonCerrarMenu.onclick = () => {
+  overlay.classList.add("ocultar")
+}
+
+gatito.onclick = () => {
+  alert("miau")
+}
